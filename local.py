bc9c8745a8649f39397514c19358ec51971e7870
Harish <harishmogalipuvvu@gmail.com>
2021-04-03 16:41:38 +0530

    Integrating a database through Xampp

--- local.py
+++ local.py
@@ -1,17 +1,51 @@
 from flask import Flask, render_template
+import mysql.connector
+
+
+
+
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-   return render_template("index.html")
+   mydb = mysql.connector.connect(
+   host="localhost",
+   user="root",
+   password="",
+   database="webdemo"
+   )
+   mycursor = mydb.cursor()
+   mycursor.execute("SELECT * FROM intro")
+   myresult = mycursor.fetchall()
+   return render_template("index.html", myresult=myresult)
+   # return "hello world"
 
 @app.route('/examrc')
 def exam_resources():
-   return render_template("indexx.html")
+   mydb = mysql.connector.connect(
+   host="localhost",
+   user="root",
+   password="",
+   database="webdemo"
+   )
+   mycursor = mydb.cursor()
+   mycursor.execute("SELECT * FROM examres")
+   myresult = mycursor.fetchall()
+   return render_template("indexx.html", myresult=myresult)
 
-@app.route("/biblerc")
-def  bible_resources():
-   return render_template("indexxx.html")
+
+@app.route("/gamerc")
+def  gamer_resources():
+   mydb = mysql.connector.connect(
+   host="localhost",
+   user="root",
+   password="",
+   database="webdemo"
+   )
+   mycursor = mydb.cursor()
+   mycursor.execute("SELECT * FROM games")
+   myresult = mycursor.fetchall()
+   return render_template("indexxx.html", myresult=myresult)
 
 if __name__ == '__main__':
    app.run(debug=True)
